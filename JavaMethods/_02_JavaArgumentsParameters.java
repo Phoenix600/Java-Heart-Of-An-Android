97bee34a662454d262f51883143f84643203ccef
Phoenix600 <pranayramteke613@gmail.com>
2022-10-18 17:49:03 +0530

    Finished working with methods

--- JavaMethods/_02_JavaArgumentsParameters.java
+++ JavaMethods/_02_JavaArgumentsParameters.java
@@ -1,10 +1,13 @@
 public class _02_JavaArgumentsParameters {
 
+
+
     static void sayHello(String fname) {
         System.out.println("Hello " + fname);
     }
 
     public static void main(String... var) {
+
         sayHello("Radhe Shyan Tiwari");
     }
 }
