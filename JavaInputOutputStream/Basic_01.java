e1ec836f3e2b1a1090a93027906eb3a071483615
Phoenix600 <pranayramteke613@gmail.com>
2022-11-22 13:15:26 +0530

    Added new code files

---
+++ JavaInputOutputStream/Basic_01.java
@@ -0,0 +1,90 @@
+import java.io.*;
+
+class Read
+{
+  String str = "Getting Started with Java Input And Output streams";
+
+
+  void readerEvent()
+  {
+    byte memo_buff[] = str.getBytes();
+    ByteArrayInputStream bais = new ByteArrayInputStream(memo_buff);
+
+    int i;
+
+    while( (i=bais.read()) != -1)
+    {
+      System.out.print((char)i);
+    }
+  }
+}
+
+class Write
+{
+    String str = "Death is the only way to life";
+    
+    void WriterEvent()
+    {
+        try{
+            FileOutputStream fos = new FileOutputStream("sample.txt");
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            baos.write(str.getBytes());
+            baos.writeTo(fos);
+            baos.flush();
+            baos.close();
+
+        }catch(Exception e){
+
+          e.printStackTrace();
+        }
+    }
+}
+
+
+class MyScanner 
+{
+  String str;
+
+  void readHandler()
+  {
+    byte memo[] = str.getBytes(); 
+    ByteArrayInputStream baos = new ByteArrayInputStream(memo);
+  
+    // baos named iterator is created 
+    int i;
+
+    while((i=baos.read())!= -1)
+    {
+      System.out.print((char)i);
+    }
+
+  }
+
+  public MyScanner(String str)
+  {
+    this.str = str;
+  }
+}
+
+class Main 
+{
+  public static void main(String...args)
+  {
+
+    // Test-Code : 01 
+    // Reading the data 
+    // Read R = new Read();
+    // R.readerEvent();
+
+    // Test-Code : 02
+    // Writing the data 
+    // Write W = new Write();
+    // W.WriterEvent();
+
+  
+    MyScanner Scanner = new MyScanner("The most blessed ones, are the most cursed ones\n");
+    Scanner.readHandler();
+
+  }
+}
---
+++ JavaInputOutputStream/comments.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+
+class UnComment
+{
+  void readEvent()
+  {
+  }
+
+  void writeEvent()
+  {
+
+  }
+}
+
+
+class Main 
+{
+  public static void main(String...args)
+  {
+
+  }
+}
